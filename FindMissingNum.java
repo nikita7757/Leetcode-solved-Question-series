e84bf33bf8ccee89763a88542dd5deaec5744bff
Nikita Shedage <132663150+nikita7757@users.noreply.github.com>
2026-03-17 00:14:42 +0530

    Add FindMissingNum class to identify missing number
    Implement a method to find the missing number in a permutation array.

---
+++ FindMissingNum.java
@@ -0,0 +1,37 @@
+cimport java.util.*;
+class FindMissingNum {
+  static  int missingNum(int arr[]) {
+        
+        int n= arr.length+1;
+        
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for(int i=0; i<arr.length; i++){
+            actualSum += arr[i];
+        }
+        
+          return expectedSum-actualSum;
+    }
+    
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        
+        int arr[] = new int[size];
+        for(int i =0; i<size; i++){
+            arr[i] = sc.nextInt();
+        }
+        int result = missingNum(arr);
+        System.out.println(result);
+    }
+  
+}
+/* You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 5]
+Output: 4
+Explanation: All the numbers from 1 to 5 are present except 4.
+
+*/
